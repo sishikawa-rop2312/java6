cbac38cc7fef81557173454ed25cda050deae3a7
sishikawa-rop2312 <tukuyomi610315@gmail.com>
2024-01-10 15:48:27 +0900

    first commit

---
+++ src/comment/Kouhan.java
@@ -0,0 +1,13 @@
+package comment;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。");
+	}
+	
+	public static void showMondokoro() {
+		System.out.println("この紋所が目に。。。");
+//		Thread.sleep(3000);
+		Zenhan.doTogame();
+	}
+}
---
+++ src/comment/Zenhan.java
@@ -0,0 +1,17 @@
+package comment;
+
+public class Zenhan {
+
+	public static void main(String[] args) {
+		doWarusa();
+		doTogame();
+	}
+	
+	public static void doWarusa() {
+		System.out.println("きなこでござる。");
+	}
+	
+	public static void doTogame() {
+		System.out.println("この老いぼれの。。。");
+	}
+}
